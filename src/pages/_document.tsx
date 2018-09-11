d52e1b7f9ba20278c9772b0f51c83f5ea6a0de88
Oleksii Oliinyk <oleksii.oliinyk@unitedprint.com>
2018-09-11 12:23:41 +0200

    fixed seo in audits

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,20 @@
-import Document, { Main, NextScript } from 'next/document';
+import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
+  public static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   public render() {
     return (
       <html lang="en">
+        <Head key="viewport">
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+        </Head>
+        <Head key="meta_description">
+          <meta name="Description" content="Next.js Universal Web App" />
+        </Head>
         <body role="main">
           <Main />
           <NextScript />
