b744f3b833b8dc9b0e95fcbd8c096c3cc09a13f1
Oleksii Oliinyk <oleksii.oliinyk@unitedprint.com>
2018-11-07 10:58:15 +0100

    changed server config

--- src/server/server.config.ts
+++ src/server/server.config.ts
@@ -5,14 +5,17 @@ interface IServerConfig {
 }
 
 export default (dev: boolean): IServerConfig => {
-  const crtPath = dev ? `${__dirname}/../../server.crt` : `${__dirname}/../../../server.crt`;
-  const keyPath = dev ? `${__dirname}/../../server.key` : `${__dirname}/../../../server.key`;
+  // const crtPath = dev ? `${__dirname}/../../server.crt` : `${__dirname}/../../../server.crt`;
+  // const keyPath = dev ? `${__dirname}/../../server.key` : `${__dirname}/../../../server.key`;
   const isHeroku = (process.env.NODE && ~process.env.NODE.indexOf('heroku'));
   if (isHeroku) {
     return { useTextCompression: true };
   }
   return {
-    certificateSettings: { crtPath, keyPath },
+    certificateSettings: dev ? undefined : {
+      crtPath: `${__dirname}/../../../server.crt`,
+      keyPath: `${__dirname}/../../../server.key`,
+    },
     useTextCompression: !dev,
     cacheMap:
       dev ? undefined
